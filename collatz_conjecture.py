ebc0b7782453b316f774bf09930e1138bf40252f
Dylshanwang <dylan.dowling@gmail.com>
2020-07-02 17:33:14 +1000

    Added try/catch to make sure user enters an integer

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,25 +1,35 @@
+counter = 1
+
+
 def collatz(initial_number):
     global counter
-    int_initial_number = int(initial_number)
-    if int_initial_number % 2 == 0:
-        even_number = int_initial_number / 2
-        print(str(even_number))
-        if even_number == 1:
-            print("It took {} iterations to reach the number 1".format(counter))
+    try:
+        int_initial_number = int(initial_number)
+        if int_initial_number % 2 == 0:
+            even_number = int_initial_number / 2
+            print(str(even_number))
+            if even_number == 1:
+                print("It took {} iterations to reach the number 1".format(counter))
+            else:
+                counter += 1
+                collatz(even_number)
         else:
-            counter += 1
-            collatz(even_number)
-    else:
-        odd_number = 3 * int_initial_number + 1
-        print(str(odd_number))
-        if odd_number == 1:
-            print("It took {} iterations to reach the number 1".format(counter))
-        else:
-            counter += 1
-            collatz(odd_number)
+            odd_number = 3 * int_initial_number + 1
+            print(str(odd_number))
+            if odd_number == 1:
+                print("It took {} iterations to reach the number 1".format(counter))
+            else:
+                counter += 1
+                collatz(odd_number)
+    except ValueError:
+        number_getter()
 
 
-counter = 1
-print("Enter an integer")
-integer = input()
-collatz(integer)
+def number_getter():
+    print("Enter an integer")
+    integer = input()
+    collatz(integer)
+
+
+number_getter()
+
