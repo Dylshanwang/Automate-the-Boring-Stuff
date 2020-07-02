de916e34cc8bd189eee958421b4edbb6b8f40ed6
Dylshanwang <dylan.dowling@gmail.com>
2020-07-02 17:45:41 +1000

    Added a while loop to reduce lines of code

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,28 +1,4 @@
-counter = 1
-
-
-def collatz(initial_number):
-    global counter
-    try:
-        int_initial_number = int(initial_number)
-        if int_initial_number % 2 == 0:
-            even_number = int_initial_number / 2
-            print(str(even_number))
-            if even_number == 1:
-                print("It took {} iterations to reach the number 1".format(counter))
-            else:
-                counter += 1
-                collatz(even_number)
-        else:
-            odd_number = 3 * int_initial_number + 1
-            print(str(odd_number))
-            if odd_number == 1:
-                print("It took {} iterations to reach the number 1".format(counter))
-            else:
-                counter += 1
-                collatz(odd_number)
-    except ValueError:
-        number_getter()
+counter = 0
 
 
 def number_getter():
@@ -31,5 +7,23 @@ def number_getter():
     collatz(integer)
 
 
-number_getter()
+def collatz(initial_number):
+    global counter
+    try:
+        number = int(initial_number)
+        while number != 1:
+            if number % 2 == 0:
+                number = number / 2
+                print(str(number))
+                counter += 1
+            else:
+                number = 3 * number + 1
+                print(str(number))
+                counter += 1
+        else:
+            print("It took {} iterations to reach the number 1".format(counter))
+    except ValueError:
+        number_getter()
 
+
+number_getter()
